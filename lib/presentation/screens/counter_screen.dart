2e580054bd7de141d0ae0ffd9b09c50b3ff8ca12
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-08 21:04:10 -0600

    ClickCounter

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -2,8 +2,18 @@
 
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
-  const CounterScreen({super.key});
+class CounterScreen extends StatefulWidget {
+
+
+const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +24,20 @@ class CounterScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('10',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+          children:  [
+            Text('$clickCounter',
+                style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
             Text('Clicks',
-                style: TextStyle(fontSize: 25))
+                style: const TextStyle(fontSize: 25))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+                clickCounter ++;
+          });
+        },
         child: const Icon(Icons.plus_one),
       ),
     );
