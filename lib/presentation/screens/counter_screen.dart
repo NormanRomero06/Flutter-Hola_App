4c33bd7952c9cd00ebd240aaa7d166307b47019e
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-08 18:42:18 -0600

    floatingActionButton

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,13 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-
 class CounterScreen extends StatelessWidget {
-const CounterScreen({super.key});
+  const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Counter screen'),),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('10',
+                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Clicks',
+                style: TextStyle(fontSize: 25))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
